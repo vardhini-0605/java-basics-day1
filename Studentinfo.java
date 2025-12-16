dcbaf8f259da2bd811dec5803ff58c13ed1ab4b4
vardhini-0605 <kondabathulavardhini0@gmail.com>
2025-12-16 10:16:08 +0530

    Day-1 Student info

---
+++ Studentinfo.java
@@ -0,0 +1,13 @@
+class StudentInfo {
+    public static void main(String[] args) {
+
+        int id = 101;
+        String name = "Vardhini";
+        int marks = 85;
+
+        System.out.println("Student Details");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Marks: " + marks);
+    }
+}
