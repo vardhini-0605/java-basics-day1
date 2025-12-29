c691f3faa94c3ac7005748dc70e72a4cb97402b2
vardhini-0605 <kondabathulavardhini0@gmail.com>
2025-12-29 09:32:27 +0530

    Add files via upload

---
+++ Elseif.java
@@ -0,0 +1,15 @@
+public class Elseif {
+    public static void main(String[] args){
+        int marks=78;
+        if(marks>=90){
+            System.out.println("Grade A");
+        }else if(marks>=65){
+            System.out.println("Grade B");
+        }else if(marks>=50){
+            System.out.println("Grade C");
+        }
+        else{
+            System.out.println("Fail");
+        }
+    }
+}
